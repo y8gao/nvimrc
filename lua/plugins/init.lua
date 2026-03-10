b539078465111d0352ac3f90ecb10c139f39f2b7
Gao, Yang 2. (NSB - CN/Hangzhou) <yang.2.gao@nokia-sbell.com>
2026-03-10 12:02:08 +0800

    fix keymap for copilot autocomplete

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -207,9 +207,9 @@ return {
     "github/copilot.vim",
     event = "VeryLazy",
     config = function()
+      vim.keymap.set('i', '<C-g>', 'copilot#Accept("")',{ expr = true, replace_keycodes = false })
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_assume_mapped = true
-      vim.keymap.set("i", "<C-g>", 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
     end,
   },
 
