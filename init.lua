099a3ceb309335e034493a56c59bb713412751b1
Gao, Yang 2. (NSB - CN/Hangzhou) <yang.2.gao@nokia-sbell.com>
2026-03-10 12:13:23 +0800

    add configs for neovide

--- init.lua
+++ init.lua
@@ -1,6 +1,29 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+if vim.g.neovide then
+  vim.g.neovide_opacity = 0.9
+  vim.o.guifont = "SauceCodePro Nerd Font Mono:h12"
+  vim.g.neovide_refresh_rate = 60
+  
+  -- Smooth cursor animation
+  vim.g.neovide_cursor_animation_length = 0.1
+  vim.g.neovide_cursor_trail_length = 0.5
+  vim.g.neovide_cursor_antialiasing = true
+  
+  -- Window options
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_profiler = false
+  
+  -- Scroll animation (smooth scrolling)
+  vim.g.neovide_scroll_animation_length = 0.3
+  
+  -- Padding around editor
+  vim.g.neovide_padding_top = 10
+  vim.g.neovide_padding_bottom = 10
+  vim.g.neovide_padding_right = 10
+  vim.g.neovide_padding_left = 10
+end
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,18 @@ require "nvchad.options"
 local o = vim.o
 o.showtabline = 2
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Clipboard configuration for Neovide
+vim.opt.clipboard:append("unnamedplus")
+if vim.g.neovide then
+  -- Windows: Shift+Insert for paste
+  if vim.fn.has("win32") == 1 then
+    vim.keymap.set("i", "<S-Insert>", '<C-r>+', { noremap = true, silent = true })
+  end
+  
+  -- Mac: Cmd+V for paste
+  if vim.fn.has("mac") == 1 then
+    vim.g.neovide_input_use_logo = true
+    vim.keymap.set("i", "<D-v>", '<C-r>+', { noremap = true, silent = true })
+  end
+end
